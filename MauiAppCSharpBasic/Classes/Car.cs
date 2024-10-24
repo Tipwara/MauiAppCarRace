7fc7d8d416c73ae24d818f1f8bb78975a13c1948
Tipwara <gamtip001@gmail.com>
2024-10-24 18:40:06 +0700

    first commit

---
+++ MauiAppCSharpBasic/Classes/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppCSharpBasic.Classes;
+
+public class Car
+{ //Properties
+    public string Model;
+
+    public string Color;
+
+    public double Speed;
+
+    public double RemainingFuel;
+
+    public int TotalDistance;
+
+    public double UseFuelRate;
+    //Constructure
+    public Car(string model, double speed, double remaining_fuel) 
+    {
+        this.Model = model;
+        this.Speed = speed;
+        this.RemainingFuel = remaining_fuel;
+    }
+    //Method
+    public void CarCalculator(int Distance, int Fuel) 
+    {
+        this.TotalDistance += Distance;
+        var used_fuel = Distance * this.UseFuelRate;
+        this.RemainingFuel = RemainingFuel - used_fuel;
+    }
+
+
+    public uint TimeUseForRun(double mile) 
+    {
+        var timeuse = mile / Speed; //ผลลัพธ์จากการหาร mile/speed = double 
+        //ต้องการ convert ให้ผลลัพธ์เป็น int
+        return Convert.ToUInt16(timeuse);
+    }
+
+    public string ShowResult() 
+    {
+        var result = $"Model:{this.Model}\n Total distance:{TotalDistance}\n Remaining Fuel:{RemainingFuel}";
+        return result;
+
+    }
+}
+
---
+++ MauiAppCSharpBasic/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿
+using MauiAppCSharpBasic.Classes;
+
+namespace MauiAppCSharpBasic
+{
+    public partial class MainPage : ContentPage
+    {
+        Car audi;
+
+        Car nissan;
+
+        bool isFinish = false;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            audi = new Car("A1", 220, 20);
+            audi.UseFuelRate = 10; //ไม่กำหนดค่าผ่าน constructure
+            nissan = new Car("S1", 180, 4);
+            nissan.UseFuelRate = 8;
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!this.isFinish)
+            {
+                var screenWidth = Application.Current.MainPage.Width;
+                this.AudiRun(screenWidth);
+                this.NissanRun(screenWidth);
+                this.myLabel.Text = audi.ShowResult+"\n" + nissan.ShowResult();
+                this.isFinish = true;
+            }
+            else
+            {
+                ImageCar1.TranslateTo(0, 0, 0, Easing.Linear);
+                ImageCar2.TranslateTo(0, 0, 0, Easing.Linear);
+                this.isFinish = true;
+            }
+
+            /*ส่ง paremeter ให้ class car ใน Car.cs
+            Car audi = new Car("A1",220, 50); //สร้าง objrct audi กับ nissanโดยใช้ สิ่งที่อยู่ใน class
+            Car nissan = new Car("S1", 180, 60);
+
+            audi.CarCalculator(300, 40);
+            nissan.CarCalculator(30, 50);*/
+
+            //myLabel.Text = audi.ShowResult() + nissan.ShowResult();
+        }
+        private void AudiRun(double distance)
+        {
+            var timeuse = audi.TimeUseForRun(distance);
+            ImageCar1.TranslateTo(distance - 100, 0, timeuse * 1000, Easing.Linear);
+
+        }
+        private void NissanRun(double distance)
+        {
+            var timeuse = nissan.TimeUseForRun(distance);
+            ImageCar2.TranslateTo(distance - 100, 0, timeuse*1000, Easing.Linear);
+
+        }
+    }
+
+}
